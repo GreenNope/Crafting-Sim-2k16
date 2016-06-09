f91b36c83fe5f3539a74b4104ffc8cefff99cdc5
GreenNope <tlash1000@gmail.com>
2016-06-09 16:47:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var bones = 0;
 function addBaseMaterials() {
 bones = bones + 1;
 if (bones = 1) {bonestext = "bone";} else {bonestext = "bones";}
-document.getElementById("bones").innerHTML = "" + bones + bonestext;
+document.getElementById("bones").innerHTML = bones + bonestext;
 }
 
 
