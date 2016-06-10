a63704d5d1d61d1820236a5a76f6f78cf298cd53
GreenNope <tlash1000@gmail.com>
2016-06-10 13:53:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var bones = 0;
 
 function boneClick() {
 bones = bones + 1;
-if (bones = 1) {boneword = ' Bone';} else {boneword = ' Bones';}
+if (bones = 1) {boneword = ' Bone';} else {boneword = ' Bones';}}
 var bonesword;
 document.getElementById('bones').innerHTML = bones + boneword;
 }
