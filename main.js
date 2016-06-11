62e6d0c96480a8e1fc9d34545f01992a6032b224
GreenNope <tlash1000@gmail.com>
2016-06-11 15:52:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
 var bones = 0;
-var boneword;
-function bonePluralize(num) {
-  if (bones = 1) {
-    boneword = ' Bone';
-  } else {
-    boneword = ' Bones';
-  }
-}
-
 function boneClick() {
   bones = bones + 1;
-  bonePluralize(bones);
-  document.getElementById('bones').innerHTML = bones + boneword;
+  document.getElementById('bones').innerHTML = 'Bones:' + bones;
 }
 
 
