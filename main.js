1905d9b90f5d6672366152d59da9a7a90cebff44
GreenNope <tlash1000@gmail.com>
2016-06-11 15:56:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var traps = 1;
 function boneClick() {
   if (traps < 0) {
     bones = bones + 1;
+    traps = traps - 1;
     document.getElementById('bones').innerHTML = "Bones: " + bones;
   }
 }
