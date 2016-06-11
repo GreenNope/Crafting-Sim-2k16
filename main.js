1dcad514e1ee0de1960cf1ba06e20f355774dab9
GreenNope <tlash1000@gmail.com>
2016-06-11 14:48:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var bones = 0;
-function bonepluralize (num) {
+function bonepluralize(num) {
   var boneword;
   if (num = 1) {boneword = ' Bone';} else {boneword = ' Bones';}
 }
 
 function boneClick() {
-bones = bones + 1;
-bonepluralize (bones);
+  bones = bones + 1;
+  bonepluralize (bones);
 document.getElementById('bones').innerHTML = bones + boneword;
 }
 
