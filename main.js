ed12aa1c0e7089deb3df013341590d1e28892bfd
GreenNope <tlash1000@gmail.com>
2016-06-11 15:52:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var bones = 0;
 function boneClick() {
   bones = bones + 1;
-  document.getElementById('bones').innerHTML = 'Bones:' + bones;
+  document.getElementById('bones').innerHTML = 'Bones: ' + bones;
 }
 
 
